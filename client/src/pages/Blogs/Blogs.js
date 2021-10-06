4b9d3c165c1ee3aade478a59af7fe385929df85d
ba144220 <hs41511@gmail.com>
2021-10-06 15:26:47 +0800

    blogs

--- client/src/pages/Blogs/Blogs.js
+++ client/src/pages/Blogs/Blogs.js
@@ -3,21 +3,47 @@ import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
 import Preview from "./Preview/Preview";
 
-// import { useTheme } from "@mui/material";
+import { Typography, useTheme } from "@mui/material";
 
 const Blogs = () => {
-    // const theme = useTheme();
+    const theme = useTheme();
+
     return (
         <Container maxWidth={"lg"}>
-            <Grid container spacing={0}>
+            <Grid container spacing={0} sx={{ position: "relative" }}>
                 <Grid
                     item
                     md={7}
                     sm={12}
                     xs={12}
-                    sx={{ backgroundColor: "black", position: "relative" }}
+                    sx={{
+                        backgroundColor: "yellow",
+                        padding: 1,
+                        paddingRight: { md: 0.5, sm: 0, xs: 0 },
+                        paddingBottom: { md: 1, sm: 0.5, xs: 0.5 },
+                        paddingLeft: 0,
+
+                        position: "relative",
+                        minHeight: "100px",
+                    }}
                 >
-                    <Preview main />
+                    <img
+                        src="https://picsum.photos/800/500"
+                        style={{ width: "100%", height: "100%", border: "solid 2px grey" }}
+                    />
+                    <div
+                        style={{
+                            position: "absolute",
+                            left: 15,
+                            bottom: 15,
+                            margin: theme.spacing(1),
+                            //backgroundColor: "lightGreen",
+                            color: "white",
+                        }}
+                    >
+                        <Typography variant="h4">我是大標題</Typography>
+                        <Typography variant="subtitle2">閱讀更多...</Typography>
+                    </div>
                 </Grid>
                 <Grid item md={5} sm={12} xs={12} sx={{ backgroundColor: "yellow" }}>
                     <Grid container spacing={0} sx={{ height: "100%" }}>
@@ -26,16 +52,85 @@ const Blogs = () => {
                             md={12}
                             sm={6}
                             xs={6}
-                            sx={{ backgroundColor: "red", position: "relative" }}
+                            sx={{
+                                backgroundColor: "red",
+
+                                minHeight: "100px",
+                                aspectRatio: "16/9",
+                                padding: 1,
+                                paddingLeft: { md: 0.5, sm: 0, xs: 0 },
+                                paddingBottom: { md: 0.5, sm: 1, xs: 1 },
+                                paddingTop: { md: 1, sm: 0.5, xs: 0.5 },
+                                paddingRight: { md: 0, sm: 0.5, xs: 0.5 },
+                                position: "relative",
+                            }}
                         >
-                            <Preview />
+                            <img
+                                src="https://picsum.photos/800/800"
+                                style={{
+                                    width: "100%",
+                                    height: "100%",
+                                    border: "solid grey 2px",
+                                }}
+                            />
+                            <div
+                                style={{
+                                    position: "absolute",
+                                    left: 10,
+                                    bottom: 10,
+                                    margin: theme.spacing(1),
+                                    //backgroundColor: "lightGreen",
+                                    color: "white",
+                                }}
+                            >
+                                <Typography variant="h4" noWrap>
+                                    我是大標題
+                                </Typography>
+                                <Typography variant="subtitle2">閱讀更多...</Typography>
+                            </div>
                         </Grid>
-                        <Grid item md={12} sm={6} xs={6} sx={{ backgroundColor: "blue" }}>
-                            <Preview />
+                        <Grid
+                            item
+                            md={12}
+                            sm={6}
+                            xs={6}
+                            sx={{
+                                backgroundColor: "blue",
+
+                                position: "relative",
+
+                                minHeight: "100px",
+                                aspectRatio: "16/9",
+
+                                padding: 1,
+                                paddingLeft: { md: 0.5, sm: 0.5, xs: 0.5 },
+                                paddingBottom: 1,
+                                paddingTop: 0.5,
+                                paddingRight: 0,
+                            }}
+                        >
+                            <img
+                                src="https://picsum.photos/600/500"
+                                style={{ width: "100%", height: "100%", border: "2px grey solid" }}
+                            />
+                            <div
+                                style={{
+                                    position: "absolute",
+                                    left: 10,
+                                    bottom: 10,
+                                    margin: theme.spacing(1),
+                                    //backgroundColor: "lightGreen",
+                                    color: "white",
+                                }}
+                            >
+                                <Typography variant="h4">我是大標題</Typography>
+                                <Typography variant="subtitle2">閱讀更多...</Typography>
+                            </div>
                         </Grid>
                     </Grid>
                 </Grid>
             </Grid>
+            <Container sx={{ width: "100%", backgroundColor: "red" }}>a</Container>
         </Container>
     );
 };
